727c6e47c3aefb1acbd1f76d0a6d455d711dc586
simao234430 <simao234430@gmail.com>
2022-07-27 10:02:07 +0800

    重命名package

---
+++ seeyoo-common/src/main/java/com/seeyoo/common/exception/DemoModeException.java
@@ -0,0 +1,15 @@
+package com.seeyoo.common.exception;
+
+/**
+ * 演示模式异常
+ * 
+ * @author yoo
+ */
+public class DemoModeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public DemoModeException()
+    {
+    }
+}
